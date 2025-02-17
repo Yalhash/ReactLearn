5715078584653d2b184e952990fbe6208fa964ac
Yalhash <yousefalhashemi20@gmail.com>
2025-02-16 22:58:49 -0500

    Initial create-react-app with a functional component

---
+++ hello_world/src/components/TestFunctional.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+
+// function TestFunctional() {
+//   return (
+//     <div>
+//       <h1>Functional Component</h1>
+//     </div>
+//   );
+// }
+
+// Example of Javascript lambda / Arrow function
+const TestFunctional = () => <h1> Lambda Functional Component </h1>
+
+export default TestFunctional
