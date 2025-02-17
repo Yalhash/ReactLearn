39340b14934337fac01536ae2f90648300bdc318
Yalhash <yousefalhashemi20@gmail.com>
2025-02-17 09:53:08 -0500

    Added more detail in how setState works

--- hello_world/src/components/StatefulClassComponent_2.js
+++ hello_world/src/components/StatefulClassComponent_2.js
@@ -12,11 +12,33 @@ class StatefulClassComponent_2 extends Component {
 
     // It seems that the "Set state" function is necessary when changing the state 
     // if you do not indicate that the state is updated, the content will not re-render
+    
+    increment() {
+        // setState is async, so the second param is a callback function executed on completion
+        /*this.setState({ numVal: this.state.numVal + 1 },
+            () => {
+                console.log("Callback value: ", this.state.numVal);
+            }
+        );
+        */
+       // Since this is async, the state value is not updated immediately
+       // We should retrieve the previous state by using the function form of setState
+       // Note that this function we pass in can also take in the props as a second argument
+       // ex: (prevState, props) => { return { numVal: prevState.numVal + props.increment } }
+       this.setState((prevState) => {
+           return {
+               numVal: prevState.numVal + 1,
+           };
+        });
+        // React would otherwise batch the state updates and cause issues.
+
+    }
+    
     render() {
         return (
             <div>
                 <h1>Count: {this.state.numVal}</h1>
-                <button onClick={() => this.setState({ numVal: this.state.numVal + 1 })}>
+                <button onClick={() => this.increment()}>
                     {this.state.ButtonText}
                 </button>
             </div>
